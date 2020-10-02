0a6252ce599015e4b4a542803fcf4de917a65cd5
Michael Henderson <mvhenderson.4@gmail.com>
2020-10-02 11:08:10 -0500

    rewrite in django

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-
-
-if __name__ == '__main__':
-    ticket = Ticket()
-# use python path properly
--- app/models/status.py
+++
@@ -1,8 +0,0 @@
-from db import Base
-import sqlalchemy as sa
-
-class Status(Base):
-    __tablename__ = 'status'
-
-    id = sa.Column(sa.BigInteger, autoincrement=True, primary_key=True)
-    name = sa.Column(sa.Unicode(100), nullable=False)
