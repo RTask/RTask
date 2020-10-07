5b533fd1b7bd66b1c6983c06b9489ce26393ae33
Yogesh More <more.yogesh7197@gmail.com>
2020-10-07 10:42:57 +0530

    comments modified

--- rtask/tickets/forms.py
+++ rtask/tickets/forms.py
@@ -1,9 +1,7 @@
-
-# brightness_5
 # import form class from django
 from django import forms
 
-# import GeeksModel from models.py
+# import Ticket from models.py
 from .models import Ticket
 class TicketForm(forms.ModelForm):
     # specify the name of model to use
--- rtask/tickets/urls.py
+++ rtask/tickets/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from.views import ticket_view
 
 urlpatterns =[
-    url('createticket', ticket_view, name='Current user'),  # get the current user
+    url('createticket', ticket_view, name='Create Ticket'),  # create ticket
 ]
