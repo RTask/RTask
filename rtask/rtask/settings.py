eb7513a2195305b504f36c844a4abe0c5a258553
Yogesh More <more.yogesh7197@gmail.com>
2020-10-07 10:48:23 +0530

    modified settings.py

--- rtask/rtask/settings.py
+++ rtask/rtask/settings.py
@@ -12,11 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR =
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'qgw%q&l*_*^pa5r&%82cz4&-71!lhhfe3ujc!z(j(y27@50iiz'
